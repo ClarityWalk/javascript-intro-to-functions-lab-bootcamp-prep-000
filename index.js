38a34930da2186331ae437f0149b4706d449c969
Chris <montgomeryroth@gmail.com>
2018-06-03 22:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,18 +18,17 @@ function sayHitToGrandma(string){
   return string
 }
 
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
-
+function sayHitToGrandma(string){
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  var mixedcase "I love you, Grandma."
+  
+  if (string === uppercase){
+    return "YES INDEED!"
+  } 
+ else if (string === mixedcase){
+   return "I love you, too."
+ }
+ else if (sting === lowercase)
+   return "I can\'t hear you!"
+}
